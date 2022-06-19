b699af8c34fe47bb9dfd4750eb3e6d0cb5c2ba94
aguy tech <aguytech@free.fr>
2022-06-19 09:27:24 +0200

    nbd: - add nbd.sh to mount/umount files - modidy tunar custom actions

--- conf/foralyse/nbd.sh
+++ conf/foralyse/nbd.sh
@@ -10,17 +10,16 @@ Mounts disk file"
 __mod_nbd() {
 	# no module nbd
 	! modinfo nbd >/dev/null 2>&1 && echo "Unable to find module nbd" && exit 1
-	lsmod 2>/dev/null | grep -q ^nbd || sudo modprobe nbd
+	lsmod 2>/dev/null | grep -q ^nbd || modprobe nbd
 }
 
 __mount_nbd() {
-	[ -f ${filelist} ] || touch ${filelist}
 	# already mounted
 	grep -q "${file}$" ${filelist} && echo "${file} already mounted in $(grep "${file}$" ${filelist} | cut -f1)" && return
 
 	for block in $(ls -d /sys/class/block/nbd[0-9] | sort) ; do 
 		if [ $(cat ${block}/size) = 0 ]; then
-			if sudo qemu-nbd -c /dev/${block##*/} "${file}" -f qcow2; then
+			if qemu-nbd -c /dev/${block##*/} "${file}" -f qcow2; then
 				echo -e "/dev/${block##*/}\t${file}" >> ${filelist}
 			else
 				echo "Unable to mount ${file} in /dev/${block##*/}"
@@ -35,7 +34,7 @@ __umount_nbd() {
 	nbd=$(grep "$1$" ${filelist} | cut -f1)
 
 	if [ "${nbd}" ] && [ $(cat /sys/class/block/${nbd#/dev/}/size) -gt 0 ]; then
-		if sudo qemu-nbd -d ${nbd}; then
+		if qemu-nbd -d ${nbd}; then
 			sed -i "\|$1$|d" ${filelist}
 		fi
 	fi
@@ -49,7 +48,7 @@ __mount_dev() {
 		path="${path_base_nbd}/${nbd#/dev/}"
 		[ -d "${path}" ] || mkdir -p ${path}
 		grep -q ${nbd} /proc/mounts && echo "${nbd} already mounted" && continue
-		sudo mount ${nbd} ${path} && echo "${nbd} mounted"
+		mount ${nbd} ${path} && echo "${nbd} mounted"
 	done
 }
 
@@ -59,7 +58,7 @@ __umount_dev() {
 
 	for nbd in $(ls -d1 ${nbd_base}* | grep -v "^${nbd_base}$" ); do
 		path="${path_base_nbd}/${nbd#/dev/}"
-		if grep -q ${nbd} /proc/mounts && sudo umount ${path}; then
+		if grep -q ${nbd} /proc/mounts && umount ${path}; then
 			echo "${nbd} unmounted"
 			rmdir ${path}
 		fi
@@ -72,11 +71,21 @@ __file() {
 	file="$1"
 }
 
-[ "$#" -lt 2 ] && echo "Wrong parameters numbers: $#" && __usage
+__init() {
+	# variables
+	script=${0##*/}
+	filelist=/tmp/${script}
+	path_base_nbd=/vms/nbd
 
-script=${0##*/}
-filelist=/tmp/${script}
-path_base_nbd=/vms/nbd
+	# filelist
+	[ -f ${filelist} ] || touch ${filelist}
+	# root privileges
+	[ "${USER}" != root  ] && echo "Root privileges are needed" && __usage
+	# Wrong parameters numbers
+	[ "$#" -lt 2 ] && echo "Wrong parameters numbers: $#" && __usage
+}
+
+__init $*
 
 case $1 in
 	mount)
