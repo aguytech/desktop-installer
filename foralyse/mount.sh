8197749d89712002076432adf67a08b2d2917c53
aguy tech <aguytech@free.fr>
2022-06-22 15:10:44 +0200

    mount cases: adjust parameters for script

--- foralyse/mount.sh
+++ foralyse/mount.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-case $1 in
+action=s1 && shift
+
+case ${action} in
 	start)
 		[ -z "$2" ] && echo "No device given to mount.sh" && exit 1
 		if blkid | grep -q "LABEL=\"$2\"" && grep -q "^UUID=.* */$2 *" /etc/fstab; then
-			mount /cases
+			mount $2
 		fi
 	;;
 
 	stop)
-		umount /cases
+		umount $2
 	;;
 	*)
 		echo "No command given to mount.sh"
